fff2a6ba5f037554fdc279d08d4497d01b2a84dd
Asano1989 <asano1989.b@gmail.com>
2025-12-02 10:21:01 +0900

    add: TailwindCSSの導入

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
 //= link_tree ../builds
 //= link_directory ../../javascript/controllers .js
 //= link_tree ../../../vendor/javascript .js
