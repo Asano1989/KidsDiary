daae6c9d1e495b77cf8bbd245c58f9c684394bcb
Asano1989 <asano1989.b@gmail.com>
2025-12-02 11:11:09 +0900

    close #54

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,8 +3,8 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <p class="text-3xl font-bold underline">Sample React (App.jsx)</p>
-      <button class="bg-gray-900 hover:bg-gray-800 text-white rounded px-4 py-2">tailwind</button>
+      <p class="text-3xl font-bold underline m-4">Sample React (App.jsx)</p>
+      <button class="bg-gray-900 hover:bg-gray-800 text-white rounded px-4 py-2 m-4">tailwind</button>
     </>
   )
 };
