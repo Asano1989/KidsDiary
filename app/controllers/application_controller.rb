70cf93ec70e6f9d0948eb132a812c6e4dcc56e8b
Asano1989 <asano1989.b@gmail.com>
2025-12-02 11:45:29 +0900

    update: allow_browserを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  SUPPORT_BROWSER_VERSIONS = {safari: 16.4, firefox: 121, chrome: all, edge: all, opera: all, ie: false}
+  allow_browser versions: SUPPORT_BROWSER_VERSIONS
 end
