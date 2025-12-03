31e6d15e60c979544123f53c4479e61f75d73656
Asano1989 <asano1989.b@gmail.com>
2025-12-03 15:26:05 +0900

    fix: 許可するホストを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.hosts << "h73a.com"
   config.hosts << "tsumugi.h73a.com"
 end
