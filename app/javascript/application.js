21ea013bf9ec3fabef62795278e74104b821f518
Asano1989 <asano1989.b@gmail.com>
2025-12-11 22:56:32 +0900

    delete: 余分なimport文を削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "./controllers"
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./react/pages/App";
