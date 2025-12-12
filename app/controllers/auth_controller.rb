93aa8fe388d9bfc1dc7c3cd47a2272792c22a7dc
Asano1989 <asano1989.b@gmail.com>
2025-12-12 15:41:15 +0900

    update: サーバーサイドからのログアウト機能を実装

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,15 @@
 class AuthController < ApplicationController
   def index
   end
+
+  def destroy
+    # 1. Rails側で使用しているCookieキーを削除
+    cookies.delete(:rails_access_token, domain: :all) # :domain => :all は必要に応じて
+    
+    # 2. フラッシュメッセージを設定
+    flash[:notice] = "ログアウトしました。"
+    
+    # 3. ログインページへリダイレクト
+    redirect_to root_path
+  end
 end
