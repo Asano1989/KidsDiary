5c2b9a15c08e0c39c7414bf4dca1a36dafa62621
Asano1989 <asano1989.b@gmail.com>
2025-12-17 21:26:04 +0900

    add: フラッシュメッセージの表示を実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then "bg-green-100 border border-green-400"
+    when :alert  then "bg-red-100 border border-red-400"
+    when :error  then "bg-yellow-100 border border-yellow-400"
+    else "bg-gray-100 border border-gray-400"
+    end
+  end
 end
