126713653483544f0c7589e474dc7dc26c5d4094
Asano1989 <asano1989.b@gmail.com>
2025-12-17 20:22:32 +0900

    add: UserはFamilyに所属するか0または1つのFamilyを作成・管理できることを定義

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,6 @@
 class Family < ApplicationRecord
+  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  has_many :users
+
   validates :name,    length: { in: 1..50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  belongs_to :family, optional: true
+  has_one :owned_family, class_name: 'Family', foreign_key: 'owner_id'
+
   validates :supabase_uid, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
