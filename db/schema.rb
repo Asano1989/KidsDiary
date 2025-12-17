9c2f0590cd6365b4546a912a0b6396b96069366e
Asano1989 <asano1989.b@gmail.com>
2025-12-17 20:29:54 +0900

    add: usersテーブルにfamily_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_17_111130) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_17_112421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,8 +29,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_17_111130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "supabase_uid"
+    t.bigint "family_id"
+    t.index ["family_id"], name: "index_users_on_family_id"
     t.index ["supabase_uid"], name: "index_users_on_supabase_uid", unique: true
   end
 
   add_foreign_key "families", "users"
+  add_foreign_key "users", "families"
 end
