5d7d3707e07f8c32b4559486da60644e76ba724d
Asano1989 <asano1989.b@gmail.com>
2025-12-19 14:15:40 +0900

    add: avatarを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   belongs_to :family, optional: true
   has_one :owned_family, class_name: 'Family', foreign_key: 'owner_id'
+  has_one_attached :avatar
 
   validates :supabase_uid, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
