8df910e666368793b9a18fafb18e85648b7f5c91
Asano1989 <asano1989.b@gmail.com>
2025-12-21 07:43:39 +0900

    update: 開発環境でletter_opener_webを使用する設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,4 +77,10 @@ Rails.application.configure do
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 
   config.active_storage.service = :cloudflare
+
+  # 送信方法をletter_opener_webに指定
+  config.action_mailer.delivery_method = :letter_opener_web
+
+  # メールのリンク生成に使用するホスト設定
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 end
