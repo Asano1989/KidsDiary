e2fddc74a6aa5fccc4cac7fdc17b41dd5e850bd6
Asano1989 <asano1989.b@gmail.com>
2025-12-21 07:51:19 +0900

    update: 暫定的なパスワードリセットメールの本文を記述

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,7 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.reset_password_email.subject
-  #
-  def reset_password_email
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def reset_password_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'おもいでつむぎ：パスワード再設定のご案内')
   end
 end
