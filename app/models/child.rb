d64ba9f27b13e815ecd8b70f804badba91bf2e2e
Asano1989 <asano1989.b@gmail.com>
2025-12-24 16:23:45 +0900

    update: DiaryChildとのアソシエーションを規定

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,6 +1,9 @@
 class Child < ApplicationRecord
   belongs_to :family
 
+  has_many :diary_children, dependent: :destroy
+  has_many :diaries, through: :diary_children
+
   validates :name, presence: true
   validates :birthday, presence: true
 end
